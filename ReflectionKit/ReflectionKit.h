43bba82c54ee740b758634d0512b64814285f413
Maxthon Chan <xcvista@me.com>
2015-12-17 15:42:33 +0800

    Initial Commit

---
+++ ReflectionKit/ReflectionKit.h
@@ -0,0 +1,19 @@
+//
+//  ReflectionKit.h
+//  ReflectionKit
+//
+//  Created by Maxthon Chan on 12/17/15.
+//  Copyright © 2015 DreamCity. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReflectionKit.
+FOUNDATION_EXPORT double ReflectionKitVersionNumber;
+
+//! Project version string for ReflectionKit.
+FOUNDATION_EXPORT const unsigned char ReflectionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReflectionKit/PublicHeader.h>
+
+
